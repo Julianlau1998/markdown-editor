e711a2fed37d64ace1c7684e50e4b7cf61125905
Julian Lau <julian.lau@aol.com>
2022-04-17 23:39:56 +0200

    scroll up on ios when onscreen keyboard opens

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -98,6 +98,16 @@ export default {
         }
     },
     created () {
+        window.visualViewport.addEventListener(
+            'resize', 
+            () => {
+                if (this.iOS) {
+                    setTimeout(() => {
+                        window.scrollTo(0)
+                    }, 200)
+                }
+            } 
+        )
         this.shareAvailable = window.navigator.share
     },
     methods: {

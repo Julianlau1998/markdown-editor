3fccd3dac53eeb108f3f21c53f1436c356013764
julianlau1998 <Julian.lau@aol.com>
2023-02-19 17:50:00 +0100

    edited ad language

--- src/components/ads/AdCard.vue
+++ src/components/ads/AdCard.vue
@@ -10,7 +10,7 @@ export default {
   name: "AdCard",
   computed: {
     adLink () {
-      return '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=3&p=288&l=ur1&category=prime_video&banner=1WX8KD0X5SW5JVYH79G2&f=ifr&linkID={{link_id}}&t=jlapps-21&tracking_id=jlapps-21" width="320" height="50" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>'
+      return '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=2&p=42&l=ur1&category=amazongeneric&banner=1SSZXMV91WA8E5ZECC02&f=ifr&linkID=5adc81a760a4f5eead519a4e25dbdbf7&t=jlapps08-21&tracking_id=jlapps08-21" width="234" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>'
     }
   },
 }

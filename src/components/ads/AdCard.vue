aa88e52c203c7c642de08c4156216857f63f60ae
julianlau1998 <Julian.lau@aol.com>
2023-03-11 13:28:34 +0100

    changed banner ad

--- src/components/ads/AdCard.vue
+++ src/components/ads/AdCard.vue
@@ -10,7 +10,7 @@ export default {
   name: "AdCard",
   computed: {
     adLink () {
-      return '<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=2&p=42&l=ur1&category=amazongeneric&banner=1SSZXMV91WA8E5ZECC02&f=ifr&linkID=5adc81a760a4f5eead519a4e25dbdbf7&t=jlapps08-21&tracking_id=jlapps08-21" width="234" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>'
+      return '<iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=288&l=ur1&category=primevideo&banner=121YK2TVQY3WK6QWFBR2&f=ifr&linkID=9a912221d6d751fbfa9685a082f45188&t=jlapps08-20&tracking_id=jlapps08-20" width="320" height="50" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>'
     }
   },
 }

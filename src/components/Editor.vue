b09fae867b00123562c3b0d3b4066b3854674105
julianlau1998 <Julian.lau@aol.com>
2024-09-13 17:03:22 +0200

    hide app store button in pro version

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -117,7 +117,7 @@ export default {
             return this.$store.state.inputFile
         },
         iosLiteApp () {
-          return window.webkit && window.webkit.messageHandlers
+          return window.webkit && window.webkit.messageHandlers.openAppStore
         }
     },
     created () {
